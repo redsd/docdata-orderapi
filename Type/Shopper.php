4eff8a60b9b0b7c5a70487d50a2ae3270c79ff61
Cas Leentfaar <git@casleentfaar.com>
2014-08-27 16:00:55 +0200

    added ipaddress to shopper

--- Type/Shopper.php
+++ Type/Shopper.php
@@ -3,55 +3,47 @@
 namespace CL\DocData\Component\OrderApi\Type;
 
 /**
- * @author Tijs Verkoyen <php-docdatapayments@verkoyen.eu>
+ * @author          Cas Leentfaar <info@casleentfaar.com>
+ * @original_author Tijs Verkoyen <php-docdatapayments@verkoyen.eu>
  */
 class Shopper extends AbstractObject
 {
     /**
-     * Shopper's full name.
-     *
+     * @var string
+     */
+    protected $id;
+
+    /**
      * @var Name
      */
     protected $name;
 
     /**
-     * Shopper's e-mail address.
-     *
      * @var string
      */
     protected $email;
 
     /**
-     * Shopper's preferred language.
-     *
      * @var Language
      */
     protected $language;
 
     /**
-     * Shopper's gender.
-     *
      * @var string
      */
     protected $gender = 'U';
 
     /**
-     * Shopper's date of birth.
-     *
      * @var string
      */
     protected $dateOfBirth;
 
     /**
-     * Shopper's phone number.
-     *
      * @var string
      */
     protected $phoneNumber;
 
     /**
-     * Shopper's mobile phone number.
-     *
      * @var string
      */
     protected $mobilePhoneNumber;
@@ -59,22 +51,38 @@ class Shopper extends AbstractObject
     /**
      * @var string
      */
-    protected $id;
+    protected $ipAddress;
 
     /**
-     * @param string $dateOfBirth
+     * @param string $id
      */
-    public function setDateOfBirth($dateOfBirth)
+    public function setId($id)
     {
-        $this->dateOfBirth = $dateOfBirth;
+        $this->id = $id;
     }
 
     /**
      * @return string
      */
-    public function getDateOfBirth()
+    public function getId()
     {
-        return $this->dateOfBirth;
+        return $this->id;
+    }
+
+    /**
+     * @param Name $name
+     */
+    public function setName(Name $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return Name
+     */
+    public function getName()
+    {
+        return $this->name;
     }
 
     /**
@@ -110,22 +118,6 @@ class Shopper extends AbstractObject
     }
 
     /**
-     * @param string $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * @return string
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
      * @param Language $language
      */
     public function setLanguage(Language $language)
@@ -158,22 +150,6 @@ class Shopper extends AbstractObject
     }
 
     /**
-     * @param Name $name
-     */
-    public function setName(Name $name)
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @return Name
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
      * @param string $phoneNumber
      */
     public function setPhoneNumber($phoneNumber)
@@ -187,5 +163,37 @@ class Shopper extends AbstractObject
     public function getPhoneNumber()
     {
         return $this->phoneNumber;
+    }
+
+    /**
+     * @param string $dateOfBirth
+     */
+    public function setDateOfBirth($dateOfBirth)
+    {
+        $this->dateOfBirth = $dateOfBirth;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateOfBirth()
+    {
+        return $this->dateOfBirth;
+    }
+
+    /**
+     * @param string $ipAddress
+     */
+    public function setIpAddress($ipAddress)
+    {
+        $this->ipAddress = $ipAddress;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIpAddress()
+    {
+        return $this->ipAddress;
     }
 }
